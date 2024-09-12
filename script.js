dae4ebef3b80f23032c6378254a001db34bfcdfc
PuneethKumar01 <puneethkumar1405@gmail.com>
2024-09-12 23:07:11 +0530

    stable update

--- script.js
+++ script.js
@@ -5,14 +5,22 @@ let win = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1,
 let turn = 0;
 let num;
 
-if (localStorage.getItem('x') == null || localStorage.getItem('o') == null) {
+let StorageValue = () => {
     localStorage.setItem('x', '0');
     localStorage.setItem('o', '0');
 }
 
+if (localStorage.getItem('x') == null || localStorage.getItem('o') == null) {
+    StorageValue();
+}
 
-document.querySelector('.x-score').innerText = localStorage.getItem('x');
-document.querySelector('.o-score').innerText = localStorage.getItem('o');
+let scoreDisplay = () => {
+    document.querySelector('.x-score').innerText = localStorage.getItem('x');
+    document.querySelector('.o-score').innerText = localStorage.getItem('o');
+}
+
+scoreDisplay();
+
 
 const write = (xo) => {
     array.push(num);
@@ -51,40 +59,91 @@ const winnerSlash = (a) => {
     }
 }
 
+let playXTurn = () => {
+    document.querySelector('#player-o-head').classList.remove('head-light')
+    document.querySelector('#player-x-head').classList.add('head-light')
+}
+
+let playOTurn = () => {
+    document.querySelector('#player-x-head').classList.remove('head-light')
+    document.querySelector('#player-o-head').classList.add('head-light')
+}
+
+let matchOn = true;
+
+playXTurn();
 
 let btn = document.querySelector('.container').addEventListener('click', (e) => {
-    click = e.target;
-    num = Number(click.getAttribute('id').charAt(3));
-    if (!array.includes(num)) {
-        if (turn < 9) {
+    if (matchOn) {
+        click = e.target;
+        num = Number(click.getAttribute('id').charAt(3));
+        if (!array.includes(num)) {
+            if (turn < 9) {
+                if (turn % 2 == 0) {
+                    write('X');
 
-   
+                    if (turn >= 4) {
+                        for (let i = 0; i < 8; i++) {
+                            if (x.includes(win[i][0]) && x.includes(win[i][1]) && x.includes(win[i][2])) {
+                                winnerSlash(win[i][0].toString() + win[i][1] + win[i][2]);
+                                document.getElementById('x-won').style.display = 'block';
+                                localStorage.setItem('x', (Number(localStorage.getItem('x')) + 1).toString());
+                                scoreDisplay();
+                                matchOn = false;
 
-            if (turn % 2 == 0) {
-                write('X');
-
-                if (turn >= 4) {
-                    for (let i = 0; i < 8; i++) {
-                        if (x.includes(win[i][0]) && x.includes(win[i][1]) && x.includes(win[i][2])) {
-                            winnerSlash(win[i][0].toString() + win[i][1] + win[i][2]);
-                            document.getElementById('winner').style.display = 'block';
-                            document.getElementById('winner').innerText = 'X won the match. click to play again'
+                            }
                         }
                     }
-                }
-            } else {
-                write('O');
-                if (turn >= 5) {
-                    for (let i = 0; i < 8; i++) {
-                        if (o.includes(win[i][0]) && o.includes(win[i][1]) && o.includes(win[i][2])) {
-                            winnerSlash(win[i][0].toString() + win[i][1] + win[i][2]);
-                            document.getElementById('winner').style.display = 'block';
-                            document.getElementById('winner').innerText = 'O won the match. click to play again'
+                    playOTurn();
+                } else {
+                    write('O');
+                    if (turn >= 5) {
+                        for (let i = 0; i < 8; i++) {
+                            if (o.includes(win[i][0]) && o.includes(win[i][1]) && o.includes(win[i][2])) {
+                                winnerSlash(win[i][0].toString() + win[i][1] + win[i][2]);
+                                document.getElementById('o-won').style.display = 'block';
+                                localStorage.setItem('o', (Number(localStorage.getItem('o')) + 1).toString());
+                                scoreDisplay();
+                                matchOn = false;
+
+                            }
                         }
                     }
+                    playXTurn();
                 }
+                turn++;
             }
-            turn++;
         }
     }
+    if ((turn == 9) && (matchOn)) {
+        document.getElementById('draw').style.display = 'block';
+    }
+})
+
+let allBox = document.querySelectorAll('.box');
+
+let clearBox = () => {
+    allBox.forEach((value) => { value.innerText = '' })
+    turn = 0;
+    array = [];
+    x = []
+    o = []
+    slashLine.className = '';
+    matchOn = true;
+    document.getElementById('x-won').style.display = 'none';
+    document.getElementById('o-won').style.display = 'none';
+    document.getElementById('draw').style.display = 'none';
+    document.querySelector('#player-x-head').classList.remove('head-light')
+    document.querySelector('#player-o-head').classList.remove('head-light')
+}
+
+document.querySelector('.reset-score').addEventListener('click', () => {
+    clearBox();
+    StorageValue();
+    scoreDisplay();
+})
+
+
+document.querySelector('.new-game').addEventListener('click', () => {
+    clearBox();
 })
